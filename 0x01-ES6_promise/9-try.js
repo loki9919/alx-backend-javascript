0e3d41e57d28c34287883fae48016a5553c85cd6
lokmane rouijel <lokmane9919@gmail.com>
2024-08-08 03:10:26 +0100

    Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+        const result = mathFunction();
+        queue.push(result);
+    } catch (e) {
+        queue.push(`Error: ${e.message}`);
+    } finally {
+        queue.push('Guardrail was processed');
+    }
+    return queue;
+}
