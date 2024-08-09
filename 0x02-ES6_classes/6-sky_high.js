f3ec80397a7ed79cb29f8d8836aefe67711e6e06
lokmane rouijel <lokmane9919@gmail.com>
2024-08-09 03:48:18 +0100

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,15 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+    // Getter for floors
+    get floors() {
+        return this._floors;
+    }
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
+}
